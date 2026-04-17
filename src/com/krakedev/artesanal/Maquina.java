fce4898ccffa5b7d17a2ba18e228bd239fdcd16e
luisjimenezgx@gmai.com <luisjimenezgx@gmail.com>
2026-04-16 21:06:26 -0400

    feat(Maquina): add attributes, getters and setters

--- src/com/krakedev/artesanal/Maquina.java
+++ src/com/krakedev/artesanal/Maquina.java
@@ -1,5 +1,38 @@
 package com.krakedev.artesanal;
 
 public class Maquina {
+	private String nombreCerveza;
+	private String descripcion;
+	public double precioPorMl;
+	public double capacidadMaxima;
+	public double cantidadActual;
+	
+	public String getNombreCerveza() {
+		return nombreCerveza;
+	}
+	public void setNombreCerveza(String nombreCerveza) {
+		this.nombreCerveza = nombreCerveza;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public double getPrecioPorMl() {
+		return precioPorMl;
+	}
+	public void setPrecioPorMl(double precioPorMl) {
+		this.precioPorMl = precioPorMl;
+	}
+	public double getCapacidadMaxima() {
+		return capacidadMaxima;
+	}
+	public void setCapacidadMaxima(double capacidadMaxima) {
+		this.capacidadMaxima = capacidadMaxima;
+	}
+	public double getCantidadActual() {
+		return cantidadActual;
+	}
 
 }
