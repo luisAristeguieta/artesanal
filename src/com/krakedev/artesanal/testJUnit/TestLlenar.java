9dddfb3b9275460f648a361e1f41de2f2ad0a25a
luisjimenezgx@gmai.com <luisjimenezgx@gmail.com>
2026-04-17 23:57:27 -0400

    test(TestLlenar): verify llenarMaquina method

--- src/com/krakedev/artesanal/testJUnit/TestLlenar.java
+++ src/com/krakedev/artesanal/testJUnit/TestLlenar.java
@@ -1,5 +1,21 @@
 package com.krakedev.artesanal.testJUnit;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import com.krakedev.artesanal.Maquina;
+
 public class TestLlenar {
+
+	@Test
+	public void testLlenarMaquina() {
+		Maquina honey = new Maquina("Honey", "Sabor a miel", 0.2, 5000);
+
+		honey.llenarMaquina();
+
+		assertEquals(4900, honey.getCantidadActual(), 0.0001);
+
+	}
 
 }
