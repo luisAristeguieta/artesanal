3d16bdb1a987525604253ad158b17af3721026e6
luisjimenezgx@gmai.com <luisjimenezgx@gmail.com>
2026-04-18 00:17:58 -0400

    test(TestRecargarJUnit): add recarga success and failure tests

---
+++ src/com/krakedev/artesanal/testJUnit/TestRecargarJUnit.java
@@ -0,0 +1,36 @@
+package com.krakedev.artesanal.testJUnit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import com.krakedev.artesanal.Maquina;
+
+public class TestRecargarJUnit {
+	
+	@Test
+	public void testRecargaExitosa() {
+		
+		Maquina honey = new Maquina("Honey", "Sabor a miel", 0.2, 5000);
+		
+		boolean Resultado = honey.recargarCerveza(1000);
+		
+		assertTrue(Resultado);
+		assertEquals(1000, honey.getCantidadActual(), 0.0001);
+		
+	}
+	
+	@Test
+	public void testRecargaFallida() {
+		
+		Maquina honey = new Maquina("Honey", "Sabor a miel", 0.2, 5000);
+		
+		boolean Resultado = honey.recargarCerveza(10000);
+		
+		assertTrue(Resultado);
+		assertEquals(10000, honey.getCantidadActual(), 0.0001);
+		
+	}
+	
+}
