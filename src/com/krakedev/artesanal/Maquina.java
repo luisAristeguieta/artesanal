591f28d6b8da52556b99785d77e51aec7f7faa28
luisjimenezgx@gmai.com <luisjimenezgx@gmail.com>
2026-04-17 21:03:31 -0400

    feat(Maquina): add imprimir method

--- src/com/krakedev/artesanal/Maquina.java
+++ src/com/krakedev/artesanal/Maquina.java
@@ -6,33 +6,47 @@ public class Maquina {
 	public double precioPorMl;
 	public double capacidadMaxima;
 	public double cantidadActual;
-	
+
 	public String getNombreCerveza() {
 		return nombreCerveza;
 	}
+
 	public void setNombreCerveza(String nombreCerveza) {
 		this.nombreCerveza = nombreCerveza;
 	}
+
 	public String getDescripcion() {
 		return descripcion;
 	}
+
 	public void setDescripcion(String descripcion) {
 		this.descripcion = descripcion;
 	}
+
 	public double getPrecioPorMl() {
 		return precioPorMl;
 	}
+
 	public void setPrecioPorMl(double precioPorMl) {
 		this.precioPorMl = precioPorMl;
 	}
+
 	public double getCapacidadMaxima() {
 		return capacidadMaxima;
 	}
+
 	public void setCapacidadMaxima(double capacidadMaxima) {
 		this.capacidadMaxima = capacidadMaxima;
 	}
+
 	public double getCantidadActual() {
 		return cantidadActual;
+	}
+
+	public void imprimir() {
+		String mensaje = "Nombre Cerveza: " + nombreCerveza + " , Descripcion: " + descripcion + " , Precio por Ml: "
+				+ precioPorMl + " , Capacidad Maxima:" + capacidadMaxima + " , Cantidad Actual:" + cantidadActual;
+		System.out.println(mensaje);
 	}
 
 }
