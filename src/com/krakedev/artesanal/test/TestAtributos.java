b3e8b1bf62e14953272f570feef89cdb1a08e88e
luisjimenezgx@gmai.com <luisjimenezgx@gmail.com>
2026-04-17 21:39:29 -0400

    test(TestAtributos): verify Maquina constructors

--- src/com/krakedev/artesanal/test/TestAtributos.java
+++ src/com/krakedev/artesanal/test/TestAtributos.java
@@ -1,9 +1,21 @@
 package com.krakedev.artesanal.test;
 
+import com.krakedev.artesanal.Maquina;
+
 public class TestAtributos {
 
 	public static void main(String[] args) {
 		
+		Maquina ipa = new Maquina("Ginger","Sabor a gengibre",0.2);
+		
+		ipa.setNombreCerveza("Ginger V2");
+		ipa.setDescripcion("Doblemente fuerte en sabor");
+		ipa.setPrecioPorMl(0.3);
+		ipa.imprimir();
+		
+		Maquina honey = new Maquina("Honey","Sabor a miel",0.2,5000);
+		
+		honey.imprimir();
 	}
 
 }
