c9a2cf40622861a9d20f95ae024ac7e6d010c9ad
luisjimenezgx@gmai.com <luisjimenezgx@gmail.com>
2026-04-17 23:18:29 -0400

    test: add TestServir class and verify servirCerveza method

---
+++ src/com/krakedev/artesanal/test/TestServir.java
@@ -0,0 +1,27 @@
+package com.krakedev.artesanal.test;
+
+import com.krakedev.artesanal.Maquina;
+
+public class TestServir {
+
+	public static void main(String[] args) {
+		Maquina honey = new Maquina("Honey","Sabor a miel",0.2,5000);
+		
+		System.out.println("*********** Estado Inicial Honey 🍺 ***********");
+		honey.imprimir();
+		
+		// Llena la maquina de cerveza honey a maxima capacidad segura:
+		System.out.println("*********** Llenando Honey 🍺 ***********");
+		honey.llenarMaquina();
+		honey.imprimir();
+		
+		System.out.println("*********** Sirviendo 1000 ml Honey 🍺 ***********");
+		double valor = honey.servirCerveza(1000);
+		System.out.println("Valor a pagar: $ " + valor);
+		
+		System.out.println("*********** Sirviendo 2000 ml Honey 🍺 ***********");
+		double valor2 = honey.servirCerveza(2000);
+		System.out.println("Valor a pagar: $ " + valor2);
+	}
+
+}
